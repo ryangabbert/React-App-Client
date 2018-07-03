12d1ee35f751fdaff0834200f1d17ea3643fb30b
Ryan Gabbert <rgabb08@gmail.com>
2018-07-03 14:46:35 -0400

    added new url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,5 +4,7 @@ switch (window.location.hostname) {
     case 'localhost':
     APIURL = 'http://localhost:3000'
     break;
+    case 'rbg-fullstack-client.herokuapp.com':
+    APIURL = 'https://rbg-fullstack-client.herokuapp.com'
 }
 export default APIURL;
