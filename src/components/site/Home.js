a5986a7dbe3fcacc4dbf55677548571bced31eb1
Ryan Gabbert <rgabb08@gmail.com>
2018-07-04 19:53:30 -0400

    css

--- src/components/site/Home.js
+++ src/components/site/Home.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
         return (
             <div>
             <Jumbotron fluid>
-              <Container fluid>
+              <Container fluid id="jumbotron">
                 <h1 className="display-3">Welcome to Indy Soccer!</h1>
                 <img src={soccerPic} alt="jumbotronPic"/>
               </Container>
