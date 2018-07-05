9b4fd73c82e610fce5cb7f3f4c73760e604827b0
Ryan Gabbert <rgabb08@gmail.com>
2018-07-04 20:48:52 -0400

    commented out registerserviceworker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
